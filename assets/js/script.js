bcb28558806263cd073711f2f4902e28ab47ac4d
Kamal el haddadi <kamal.el.haddaadi@gmail.com>
2024-05-21 01:54:45 +0200

    random commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -315,4 +315,21 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-// google translate API Intgration
+// google Oauthentification with google and github
+const form = document.querySelector("form"),
+        nextBtn = form.querySelector(".nextBtn"),
+        backBtn = form.querySelector(".backBtn"),
+        allInput = form.querySelectorAll(".first input");
+
+
+nextBtn.addEventListener("click", ()=> {
+    allInput.forEach(input => {
+        if(input.value != ""){
+            form.classList.add('secActive');
+        }else{
+            form.classList.remove('secActive');
+        }
+    })
+})
+
+backBtn.addEventListener("click", () => form.classList.remove('secActive'));
